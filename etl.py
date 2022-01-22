bde6949aba906166b6778c74b104b7c3cae0c6ee
mikle <mihailgorbunov74@gmail.com>
2022-01-22 13:25:52 -0800

    The first basic version is done

--- etl.py
+++ etl.py
@@ -4,63 +4,109 @@ import psycopg2
 import pandas as pd
 from sql_queries import *
 
+def read_song_jsons (columns_list, files_list, debug = False):
+    '''the function reads json file(s) from the files_list and returns a data frame with columns = columns_list'''
+    data_list = []
+    j = 0
+    for a_file in files_list:
+        json_df = pd.read_json(files_list[j], lines=True, convert_axes=False, dtype=False)
+        j+=1
+        for i, row in json_df.iterrows():
+            data = row[columns_list].values.tolist()
+            if debug:
+                print(type(data[0]))
+            data_list.append(data)   
+    return(pd.DataFrame(data_list, columns = columns_list))
+
+def trim_str(string_to_trim, length):
+    '''if string_to_trim is a string and the length of it is more than "length", the function truncates it to the given length'''
+    if (isinstance(string_to_trim, str)):
+        if len(string_to_trim) > length:
+            return(string_to_trim[:length])
+        else:
+            return(string_to_trim)
+    else:
+        return(string_to_trim)
+
+def fill_table(dataframe, cursor, q, col_max_length, debug = False):
+    '''the function inserts data from the "dataframe" to the "cursor" position of a table. "col_max_length" is a list of max length for\
+    string data (for non string arguments it's ignored). "q" -- SQL query for performing insert'''
+    print(dataframe.columns)
+    for i, row in dataframe.iterrows():
+        if debug:
+            print((row))
+        row_list = row.tolist()    
+        for index, el in enumerate(row_list):
+            #print('index=',index, ' len1=',  len(row_list), ' len2=',  len(col_max_length))
+            row_list[index] = trim_str(el, col_max_length[index])
+        new_row = pd.Series(row_list)
+        cursor.execute(q, new_row) 
+
+def convert_to_datetime(ms):
+    '''the auxiary function for converting int mileseconds to pd.timestamp'''
+    df = pd.DataFrame([ms], columns = ['ms'])
+    df['timestamp'] = pd.to_datetime(df['ms'] , unit='ms')
+    return (df['timestamp'].iloc[0])
 
 def process_song_file(cur, filepath):
+    '''the function takes current cursor and the files path where it reads all JSON files and fills the song and artist tables'''
     # open song file
-    df = 
+    song_col_list = ['song_id', 'title', 'duration',  'year','artist_id']
+    artist_col_list = ['artist_id', 'artist_name', 'artist_latitude','artist_longitude', 'artist_location']
+    df = read_song_jsons (artist_col_list + ['song_id', 'title', 'duration',  'year'], [filepath])
 
     # insert song record
-    song_data = 
-    cur.execute(song_table_insert, song_data)
+    max_length = [22, 128, None,  None, 22]
+    fill_table(df[song_col_list], cur, song_table_insert, max_length)
     
     # insert artist record
-    artist_data = 
-    cur.execute(artist_table_insert, artist_data)
-
+    max_length = [22, 128, None,  None, 128]
+    fill_table(df[artist_col_list], cur, artist_table_insert, max_length)
 
 def process_log_file(cur, filepath):
+    '''the function takes current cursor and the files path where it reads all JSON files and fills the time, user and songolay tables'''
+    columns_log = ['artist', 'auth', 'firstName', 'gender', 'itemInSession', 'lastName', 'length', 'level', 'location', 'method', 'page', 'registration'\
+           ,'sessionId', 'song', 'status', 'ts', 'userAgent', 'userId']
     # open log file
-    df = 
+    df = read_song_jsons(columns_log, [filepath])
 
     # filter by NextSong action
-    df = 
+    df = df[df['page'] == 'NextSong'].copy()
+    time_df = df
 
     # convert timestamp column to datetime
-    t = 
+    time_df['timestamp'] = pd.to_datetime(time_df['ts'] , unit='ms') 
+    time_df['new_col'] = time_df['timestamp'].apply(lambda row: [row.hour, row.day, row.weekofyear, row.month, row.year, row.dayofweek])
+    time_df[['hour', 'day', 'weekofyear', 'month', 'year', 'dayofweek']]= pd.DataFrame(time_df['new_col'].tolist(), index = time_df.index)
+    columns_time =  ['sessionId', 'itemInSession', 'timestamp', 'hour', 'day', 'weekofyear', 'month', 'year', 'dayofweek']
+    max_length = [None, None, None,  None, None, None, None, None, None]
+    fill_table( time_df[columns_time], cur, time_table_insert, max_length)
     
-    # insert time data records
-    time_data = 
-    column_labels = 
-    time_df = 
-
-    for i, row in time_df.iterrows():
-        cur.execute(time_table_insert, list(row))
-
-    # load user table
-    user_df = 
-
-    # insert user records
-    for i, row in user_df.iterrows():
-        cur.execute(user_table_insert, row)
-
+    # Fill user table
+    user_columns = ['userId', 'lastName', 'gender', 'firstName', 'level']
+    user_df = df[user_columns].drop_duplicates(subset='userId')
+    max_length = [11, 64, 1,  64, 4]
+    fill_table(user_df, cur, user_table_insert, max_length)
+    
     # insert songplay records
     for index, row in df.iterrows():
-        
-        # get songid and artistid from song and artist tables
-        cur.execute(song_select, (row.song, row.artist, row.length))
+        # get songid and artistid from the song table
+        cur.execute(song_select, (row.song, float(str(row.length))))
         results = cur.fetchone()
-        
         if results:
             songid, artistid = results
         else:
             songid, artistid = None, None
-
         # insert songplay record
-        songplay_data = 
+        my_dict = row.to_dict()
+        songplay_data = (trim_str(my_dict.get('userId'), 11), trim_str(songid,22), trim_str(artistid,22), my_dict.get('sessionId')\
+                     , trim_str(my_dict.get('location'), 128), trim_str(my_dict.get('userAgent'),192), trim_str(my_dict.get('level'),4), convert_to_datetime(my_dict.get('ts')))
         cur.execute(songplay_table_insert, songplay_data)
+        
 
 
 def process_data(cur, conn, filepath, func):
+    '''the function takes the current cursor, connection, the function name, and the files path where it reads all JSON files and runs the function'''
     # get all files matching extension from directory
     all_files = []
     for root, dirs, files in os.walk(filepath):
--- sql_queries.py
+++ sql_queries.py
@@ -8,46 +8,40 @@ time_table_drop = "DROP TABLE IF EXISTS time"
 
 # CREATE TABLES
 #songplay_id, start_time, user_id, level, song_id, artist_id, session_id, location, user_agent
-songplay_table_create = "CREATE TABLE IF NOT EXISTS songplays(user_id INT, song_id INT, artist_id INT, session_id INT\
-, location VARCHAR(128), item_in_session INT, user_agent VARCHAR(128), PRIMARY KEY(item_in_session, session_id));"        
+songplay_table_create = """CREATE TABLE IF NOT EXISTS songplays (songplay_id SERIAL PRIMARY KEY, user_id VARCHAR (11), song_id VARCHAR(22), artist_id VARCHAR(22), session_id INT\
+, location VARCHAR(128), user_agent VARCHAR(192), level VARCHAR(4), start_time TIMESTAMP);"""
+  
 #user_id, first_name, last_name, gender, level
-user_table_create = "CREATE TABLE IF NOT EXISTS users(user_id INT, user_last_name VARCHAR(50), user_gender VARCHAR(1), user_first_name VARCHAR(50)\
+user_table_create = "CREATE TABLE IF NOT EXISTS users(user_id VARCHAR (11), last_name VARCHAR(64), gender VARCHAR(1), first_name VARCHAR(64)\
 , level VARCHAR(4), PRIMARY KEY(user_id));"  
 #song_id, title, artist_id, year, duration
-song_table_create = "CREATE TABLE IF NOT EXISTS songs(song_id varchar(22), song_title VARCHAR(150), artist_id INT, year INT, duration REAL, PRIMARY KEY(song_id));" 
+song_table_create = "CREATE TABLE IF NOT EXISTS songs(song_id VARCHAR(22), song_title VARCHAR(128), duration DOUBLE PRECISION, year INT\
+, artist_id VARCHAR(22), PRIMARY KEY(song_id));" 
 #artist_id, name, location, latitude, longitude
-artist_table_create = "CREATE TABLE IF NOT EXISTS artists(artist_id INT, artist_name VARCHAR(150), artist_latitude DOUBLE PRECISION\
-, artist_longitude DOUBLE PRECISION, PRIMARY KEY(artist_id));"
+artist_table_create = "CREATE TABLE IF NOT EXISTS artists(artist_id VARCHAR(22), name VARCHAR(128), latitude DOUBLE PRECISION\
+, longitude DOUBLE PRECISION, location VARCHAR(128), PRIMARY KEY(artist_id));"
 #start_time, hour, day, week, month, year, weekday
-time_table_create = "CREATE TABLE IF NOT EXISTS time(start_time TIMESTAMP, session_id INT,  item_in_session INT\
+time_table_create = "CREATE TABLE IF NOT EXISTS time(session_id INT,   item_in_session INT, start_time TIMESTAMP\
 , hour INT,  day INT, week INT, month INT, year INT, weekday INT, PRIMARY KEY(item_in_session, session_id));"
 
 # INSERT RECORDS
 
-songplay_table_insert = ("""
-""")
+songplay_table_insert = "INSERT INTO songplays VALUES(DEFAULT, %s, %s, %s, %s, %s, %s, %s, %s);"
 
-user_table_insert = ("""
-""")
+user_table_insert = "INSERT INTO users VALUES(%s, %s, %s, %s, %s) ON CONFLICT DO NOTHING;"
 
-song_table_insert = ("""
-""")
-
-artist_table_insert = ("""
-""")
+song_table_insert = "INSERT INTO songs VALUES(%s, %s, %s, %s, %s);"
 
 
-time_table_insert = ("""
-""")
+artist_table_insert = "INSERT INTO artists VALUES(%s, %s, %s, %s, %s) ON CONFLICT DO NOTHING;"
+
+
+time_table_insert = "INSERT INTO time VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s);"
 
 # FIND SONGS
 
-song_select = ("""
-""")
-
+song_select = """SELECT song_id, artist_id FROM songs WHERE song_title = %s AND duration = %s""" 
 # QUERY LISTS
 
 create_table_queries = [songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
 drop_table_queries = [songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
-#create_table_queries = [songplay_table_create, user_table_create]
-#drop_table_queries = [songplay_table_drop]
